1288ea8ea3fb6934d990481873eee6e3cb71766f
kgomotsodingiswayo <motsopetlele95@gmail.com>
2026-01-29 14:50:00 +0200

    added basic form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert("Welcome to my AI Workout Generator");
+function generateWorkout(event) {
+  event.preventDefault();
+
+let workoutelement=document.querySelector("#workout-output");
+
+
+new Typewriter("#workout-output", {
+  strings: "`To do a burpee, start standing, squat down, place hands on the floor, kick feet back to a plank, do a push-up (optional), jump feet forward, then jump up explosively, landing softly to repeat. Focus on keeping your core tight, back straight, and engaging your whole body for this full-body cardio and strength move, with variations available for all fitness levels.",
+  autoStart: true,
+  deplay: 50,
+  cursor: "|",
+});
+}
+let workoutFormElement = document.querySelector("#workout-generator-form");
+workoutFormElement.addEventListener("submit", generateWorkout);
